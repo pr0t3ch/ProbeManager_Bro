cf77244a26d31943f79e5e7da406972b44f9da70
uberkie <44478849+uberkie@users.noreply.github.com>
2020-07-16 17:40:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,165 +6,165 @@ echo '## Install Bro ##'
 arg=$1
 destfull=$2
 
-if [[ "$BRO_VERSION" == "" ]]; then
-    BRO_VERSION="2.5.3"
+if [[ "$ZEEK_VERSION" == "" ]]; then
+    ZEEK_VERSION="2.5.3"
 fi
 config=""
 rules=""
 # OSX with brew
 if [[ $OSTYPE == *"darwin"* ]]; then
     if brew --version | grep -qw Homebrew ; then
-        if ! brew list | grep -qw bro ; then
-            brew install bro
+        if ! brew list | grep -qw zeek ; then
+            brew install zeek
         fi
         config="/usr/local/etc/"
-        rules="/usr/local/opt/bro/share/bro/"
+        rules="/usr/local/opt/zeek/share/zeek/"
     fi
 elif [ -f /etc/debian_version ]; then
     # Ubuntu
     if [[ "$TRAVIS" = true ]]; then
-        if ! type bro ; then
-            curl http://download.opensuse.org/repositories/network:bro/xUbuntu_14.04/Release.key | sudo apt-key add -
-            echo "deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/ /" | sudo tee -a /etc/apt/sources.list
-            sudo apt-get update -qq
-            sudo apt-get -y --allow-unauthenticated install bro
-            export PATH=/opt/bro/bin:$PATH && export LD_LIBRARY_PATH=/opt/bro/lib/:$LD_LIBRARY_PATH
-            sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
+        if ! type zeek ; then
+            echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security:zeek.gpg > /dev/null
+sudo apt update -qq
+            sudo apt-get -y --allow-unauthenticated install zeek
+            export PATH=/opt/zeek/bin:$PATH && export LD_LIBRARY_PATH=/opt/zeek/lib/:$LD_LIBRARY_PATH
+            sudo setcap cap_net_raw,cap_net_admin=eip $( which zeek )
         fi
-        sudo chown "$SERVER_USER":"$CURRENT_USER" $( which bro )
-        sudo chown -R "$SERVER_USER":"$CURRENT_USER" /opt/bro
-        sudo chmod -R 770 /opt/bro
-        sudo chmod 750 $( which bro )
-        config="/opt/bro/etc/"
-        rules="/opt/bro/share/bro/"
+        sudo chown "$SERVER_USER":"$CURRENT_USER" $( which zeek )
+        sudo chown -R "$SERVER_USER":"$CURRENT_USER" /opt/zeek
+        sudo chmod -R 770 /opt/zeek
+        sudo chmod 750 $( which zeek )
+        config="/opt/zeek/etc/"
+        rules="/opt/zeek/share/zeek/"
     else # Debian and ubuntu
-        if ! type bro ; then
+        if ! type zeek ; then
             sudo apt update
             sudo apt install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev libgeoip-dev sendmail libcap2-bin wget curl ca-certificates
-            wget https://www.bro.org/downloads/bro-"$BRO_VERSION".tar.gz
-            tar xf bro-"$BRO_VERSION".tar.gz
-            ( cd bro-"$BRO_VERSION" && ./configure )
-            ( cd bro-"$BRO_VERSION" && make -j$(nproc)  )
-            ( cd bro-"$BRO_VERSION" && sudo make install )
-            rm bro-"$BRO_VERSION".tar.gz && sudo rm -rf bro-"$BRO_VERSION"
-            export PATH=/usr/local/bro/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/bro/lib/:$LD_LIBRARY_PATH
-            sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
+            wget https://www.zeek.org/downloads/zeek-"$ZEEK_VERSION".tar.gz
+            tar xf zeek-"$ZEEK_VERSION".tar.gz
+            ( cd zeek-"$ZEEK_VERSION" && ./configure )
+            ( cd zeek-"$ZEEK_VERSION" && make -j$(nproc)  )
+            ( cd zeek-"$ZEEK_VERSION" && sudo make install )
+            rm zeek-"$ZEEK_VERSION".tar.gz && sudo rm -rf zeek-"$ZEEK_VERSION"
+            export PATH=/usr/local/zeek/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/zeek/lib/:$LD_LIBRARY_PATH
+            sudo setcap cap_net_raw,cap_net_admin=eip $( which zeek )
         fi
         if ! type critical-stack-intel ; then
             curl --silent https://packagecloud.io/install/repositories/criticalstack/critical-stack-intel/script.deb.sh | sudo bash
             sudo apt-get install critical-stack-intel
         fi
         if [[ "$arg" = 'prod' ]]; then
-                sudo chown "$SERVER_USER":"$CURRENT_USER" $( which bro )
-                sudo chown -R "$SERVER_USER":"$CURRENT_USER" /usr/local/bro
-                sudo chown -R "$SERVER_USER":"$CURRENT_USER" /etc/bro
-                sudo chmod -R 770 /usr/local/bro
-                sudo chmod 750 $( which bro )
+                sudo chown "$SERVER_USER":"$CURRENT_USER" $( which zeek )
+                sudo chown -R "$SERVER_USER":"$CURRENT_USER" /usr/local/zeek
+                sudo chown -R "$SERVER_USER":"$CURRENT_USER" /etc/zeek
+                sudo chmod -R 770 /usr/local/zeek
+                sudo chmod 750 $( which zeek )
         else
-                sudo chown "$CURRENT_USER" $( which bro )
-                sudo chown -R "$CURRENT_USER" /usr/local/bro
+                sudo chown "$CURRENT_USER" $( which zeek )
+                sudo chown -R "$CURRENT_USER" /usr/local/zeek
         fi
-        config="/usr/local/bro/etc/"
-        rules="/etc/bro/"
+        config="/usr/local/zeek/etc/"
+        rules="/etc/zeek/"
     fi
 fi
 
-if ! type bro ; then
+if ! type zeek ; then
     exit 1
 fi
 
-which bro
-bro --version
-which broctl
-broctl --version
+which zeek
+zeek --version
+which zeekctl
+zeekctl --version
 
-echo "BRO_BINARY = '$( which bro )'" > "$destfull"probemanager/bro/settings.py
-echo "BROCTL_BINARY = '$( which broctl )'" >> "$destfull"probemanager/bro/settings.py
-echo "BRO_CONFIG = '$config'" >> "$destfull"probemanager/bro/settings.py#!/usr/bin/env bash
+echo "ZEEK_BINARY = '$( which zeek )'" > "$destfull"probemanager/zeek/settings.py
+echo "BROCTL_BINARY = '$( which zeekctl )'" >> "$destfull"probemanager/zeek/settings.py
+echo "ZEEK_CONFIG = '$config'" >> "$destfull"probemanager/zeek/settings.py#!/usr/bin/env bash
 
-echo '## Install bro ##'
+echo '## Install zeek ##'
 # Install on ProbeManager server
 # Get args
 arg=$1
 destfull=$2
 
-if [[ "$BRO_VERSION" == "" ]]; then
-    BRO_VERSION="2.5.3"
+if [[ "$ZEEK_VERSION" == "" ]]; then
+    ZEEK_VERSION="2.5.3"
 fi
 config=""
 rules=""
 # OSX with brew
 if [[ $OSTYPE == *"darwin"* ]]; then
     if brew --version | grep -qw Homebrew ; then
-        if ! brew list | grep -qw bro ; then
-            brew install bro
+        if ! brew list | grep -qw zeek ; then
+            brew install zeek
         fi
         config="/usr/local/etc/"
-        rules="/usr/local/opt/bro/share/bro/"
+        rules="/usr/local/opt/zeek/share/zeek/"
     fi
 elif [ -f /etc/debian_version ]; then
     # Ubuntu
     if [[ "$TRAVIS" = true ]]; then
-        if ! type bro ; then
-            curl -fsSL https://download.opensuse.org/repositories/security:bro/xUbuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security:bro.gpg > /dev/null
-            echo 'deb http://download.opensuse.org/repositories/security:/bro/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/security:bro.list
+        if ! type zeek ; then
+            curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security:zeek.gpg > /dev/null
+            echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
             sudo apt-get update -qq
-            sudo apt-get -y --allow-unauthenticated install bro
-            export PATH=/opt/bro/bin:$PATH && export LD_LIBRARY_PATH=/opt/bro/lib/:$LD_LIBRARY_PATH
-            sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
+            sudo apt-get -y --allow-unauthenticated install zeek
+            export PATH=/opt/zeek/bin:$PATH && export LD_LIBRARY_PATH=/opt/zeek/lib/:$LD_LIBRARY_PATH
+            sudo setcap cap_net_raw,cap_net_admin=eip $( which zeek )
         fi
-        sudo chown "$SERVER_USER":"$CURRENT_USER" $( which bro )
-        sudo chown -R "$SERVER_USER":"$CURRENT_USER" /opt/bro
-        sudo chmod -R 770 /opt/bro
-        sudo chmod 750 $( which bro )
-        config="/opt/bro/etc/"
-        rules="/opt/bro/share/bro/"
+        sudo chown "$SERVER_USER":"$CURRENT_USER" $( which zeek )
+        sudo chown -R "$SERVER_USER":"$CURRENT_USER" /opt/zeek
+        sudo chmod -R 770 /opt/zeek
+        sudo chmod 750 $( which zeek )
+        config="/opt/zeek/etc/"
+        rules="/opt/zeek/share/zeek/"
     else # Debian and ubuntu
-        if ! type bro ; then
+        if ! type zeek ; then
             sudo apt update
             sudo apt install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev libgeoip-dev sendmail libcap2-bin wget curl ca-certificates
-            wget https://download.zeek.org/bro-"$BRO_VERSION".tar.gz
-            tar xf bro-"$BRO_VERSION".tar.gz
-            ( cd bro-"$BRO_VERSION" && ./configure )
-            ( cd bro-"$BRO_VERSION" && make -j$(nproc)  )
-            ( cd bro-"$BRO_VERSION" && sudo make install )
-            rm bro-"$BRO_VERSION".tar.gz && sudo rm -rf bro-"$BRO_VERSION"
-            export PATH=/usr/local/bro/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/bro/lib/:$LD_LIBRARY_PATH
-            sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
+            wget https://download.zeek.org/zeek-"$ZEEK_VERSION".tar.gz
+            tar xf zeek-"$ZEEK_VERSION".tar.gz
+            ( cd zeek-"$ZEEK_VERSION" && ./configure )
+            ( cd zeek-"$ZEEK_VERSION" && make -j$(nproc)  )
+            ( cd zeek-"$ZEEK_VERSION" && sudo make install )
+            rm zeek-"$ZEEK_VERSION".tar.gz && sudo rm -rf zeek-"$ZEEK_VERSION"
+            export PATH=/usr/local/zeek/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/zeek/lib/:$LD_LIBRARY_PATH
+            sudo setcap cap_net_raw,cap_net_admin=eip $( which zeek )
         fi
         if ! type critical-stack-intel ; then
             curl --silent https://packagecloud.io/install/repositories/criticalstack/critical-stack-intel/script.deb.sh | sudo bash
             sudo apt-get install critical-stack-intel
         fi
         if [[ "$arg" = 'prod' ]]; then
-                sudo chown "$SERVER_USER":"$CURRENT_USER" $( which bro )
-                sudo chown -R "$SERVER_USER":"$CURRENT_USER" /usr/local/bro
-                sudo chown -R "$SERVER_USER":"$CURRENT_USER" /etc/bro
-                sudo chmod -R 770 /usr/local/bro
-                sudo chmod 750 $( which bro )
+                sudo chown "$SERVER_USER":"$CURRENT_USER" $( which zeek )
+                sudo chown -R "$SERVER_USER":"$CURRENT_USER" /usr/local/zeek
+                sudo chown -R "$SERVER_USER":"$CURRENT_USER" /etc/zeek
+                sudo chmod -R 770 /usr/local/zeek
+                sudo chmod 750 $( which zeek )
         else
-                sudo chown "$CURRENT_USER" $( which bro )
-                sudo chown -R "$CURRENT_USER" /usr/local/bro
+                sudo chown "$CURRENT_USER" $( which zeek )
+                sudo chown -R "$CURRENT_USER" /usr/local/zeek
         fi
-        config="/usr/local/bro/etc/"
-        rules="/etc/bro/"
+        config="/usr/local/zeek/etc/"
+        rules="/etc/zeek/"
     fi
 fi
 
-if ! type bro ; then
+if ! type zeek ; then
     exit 1
 fi
 
-which bro
-bro --version
-which broctl
-broctl --version
+which zeek
+zeek --version
+which zeekctl
+zeekctl --version
 
-echo "BRO_BINARY = '$( which bro )'" > "$destfull"probemanager/bro/settings.py
-echo "BROCTL_BINARY = '$( which broctl )'" >> "$destfull"probemanager/bro/settings.py
-echo "BRO_CONFIG = '$config'" >> "$destfull"probemanager/bro/settings.py
-echo "BRO_RULES = '$rules'" >> "$destfull"probemanager/bro/settings.py
-echo "BRO_VERSION = '$BRO_VERSION'" >> "$destfull"probemanager/bro/settings.py
+echo "ZEEK_BINARY = '$( which zeek )'" > "$destfull"probemanager/zeek/settings.py
+echo "BROCTL_BINARY = '$( which zeekctl )'" >> "$destfull"probemanager/zeek/settings.py
+echo "ZEEK_CONFIG = '$config'" >> "$destfull"probemanager/zeek/settings.py
+echo "ZEEK_RULES = '$rules'" >> "$destfull"probemanager/zeek/settings.py
+echo "ZEEK_VERSION = '$ZEEK_VERSION'" >> "$destfull"probemanager/zeek/settings.py
 
-echo "BRO_RULES = '$rules'" >> "$destfull"probemanager/bro/settings.py
-echo "BRO_VERSION = '$BRO_VERSION'" >> "$destfull"probemanager/bro/settings.py
+echo "ZEEK_RULES = '$rules'" >> "$destfull"probemanager/zeek/settings.py
+echo "ZEEK_VERSION = '$ZEEK_VERSION'" >> "$destfull"probemanager/zeek/settings.py
